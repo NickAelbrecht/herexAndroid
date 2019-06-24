344937d53cb25bfccfb11db0f3e9218a2a9c21a3
nick <nick.aelb@hotmail.com>
2019-06-24 18:08:37 +0200

    added basic fragments

--- app/src/main/java/com/example/nick/herexamen/MainActivity.kt
+++ app/src/main/java/com/example/nick/herexamen/MainActivity.kt
@@ -1,24 +1,27 @@
 package com.example.nick.herexamen
 
+import android.net.Uri
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
+import android.support.v4.app.Fragment
 import android.support.v7.app.AppCompatActivity
+import com.example.nick.herexamen.fragments.HomeFragment
+import com.example.nick.herexamen.fragments.LoginFragment
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), HomeFragment.OnFragmentInteractionListener, LoginFragment.OnFragmentInteractionListener {
 
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
             R.id.navigation_home -> {
-                message.setText(R.string.title_home)
+                switchFragment(HomeFragment.newInstance("test1", "test2"))
                 return@OnNavigationItemSelectedListener true
             }
-            R.id.navigation_dashboard -> {
-                message.setText(R.string.title_dashboard)
+            R.id.navigation_oefeningen -> {
                 return@OnNavigationItemSelectedListener true
             }
-            R.id.navigation_notifications -> {
-                message.setText(R.string.title_notifications)
+            R.id.navigation_login -> {
+                switchFragment(LoginFragment.newInstance("test1", "test2"))
                 return@OnNavigationItemSelectedListener true
             }
         }
@@ -31,4 +34,19 @@ class MainActivity : AppCompatActivity() {
 
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
     }
+
+    override fun onFragmentInteraction(uri: Uri) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+
+    private fun switchFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .addToBackStack(null)
+            .replace(R.id.fragment, fragment)
+            .commit()
+
+    }
+
+
 }
