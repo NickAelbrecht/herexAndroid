e3f579c7e22ae66422b5f2dd8b723237f5789e6f
NickAelbrecht <nick.aelbrecht.y9727@student.hogent.be>
2019-07-24 11:14:35 +0200

    added profilefragment

---
+++ app/src/main/java/com/example/nick/herexamen/fragments/ProfileFragment.kt
@@ -0,0 +1,117 @@
+package com.example.nick.herexamen.fragments
+
+import android.content.Context
+import android.net.Uri
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.TextView
+import com.example.nick.herexamen.MainActivity
+
+import com.example.nick.herexamen.R
+import com.example.nick.herexamen.authentication.AuthenticationService
+import kotlinx.android.synthetic.main.fragment_profile.view.*
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Activities that contain this fragment must implement the
+ * [ProfileFragment.OnFragmentInteractionListener] interface
+ * to handle interaction events.
+ * Use the [ProfileFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ *
+ */
+class ProfileFragment : Fragment() {
+
+    private var listener: OnFragmentInteractionListener? = null
+    private lateinit var authenticationService: AuthenticationService
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+        }
+        authenticationService = AuthenticationService(activity as MainActivity)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        var view = inflater.inflate(R.layout.fragment_profile, container, false)
+        view.profiel_btn_logout.setOnClickListener { logUserOut() }
+        updateUi(view)
+        return view
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    fun onButtonPressed(uri: Uri) {
+        listener?.onFragmentInteraction(uri)
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        if (context is OnFragmentInteractionListener) {
+            listener = context
+        } else {
+            throw RuntimeException("$context must implement OnFragmentInteractionListener")
+        }
+    }
+
+    private fun logUserOut() {
+        authenticationService.logUserOut()
+        (activity as MainActivity).showHome()
+    }
+
+    private fun updateUi(view: View) {
+        view.findViewById<TextView>(R.id.profiel_email).text = authenticationService.getAuth().currentUser?.email
+        view.findViewById<TextView>(R.id.profiel_naam).text = authenticationService.getAuth().currentUser?.displayName
+
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        listener = null
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     *
+     *
+     * See the Android Training lesson [Communicating with Other Fragments]
+     * (http://developer.android.com/training/basics/fragments/communicating.html)
+     * for more information.
+     */
+    interface OnFragmentInteractionListener {
+        fun onFragmentInteraction(uri: Uri)
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment ProfileFragment.
+         */
+        @JvmStatic
+        fun newInstance() =
+            ProfileFragment().apply {
+                arguments = Bundle().apply {
+                }
+            }
+    }
+}
