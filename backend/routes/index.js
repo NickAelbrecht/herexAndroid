53e33f49b242ac378368255b4914296f72e7add7
NickAelbrecht <nick.aelbrecht.y9727@student.hogent.be>
2019-08-14 11:55:06 +0200

    Added delete and getByTitle but need some work

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -6,20 +6,32 @@ require("./../models/Recipe");
 
 let Recipe = mongoose.model("Recipe");
 
-
-
 /* GET home page. */
 router.get("/", function(req, res, next) {
   res.json({ test: "server works" });
 });
 
 router.get("/recipes", function(req, res) {
-  console.log("werkt")
-  res.json({ recipes: "Bla bla recepten" });
+  let query = Recipe.find();
+  query.exec(function(err, recipes) {
+    if (err) {
+      return next(err);
+    }
+    res.json({recipes});
+  });
+});
+
+router.get("/recipes/:title", function(req, res) {
+  Recipe.findOne({title:req.params.title},function(err, recipes) {
+    if (err) {
+      return next(err);
+    }
+    res.json({recipes});
+  });
 });
 
 router.post("/recipes", function(req, res, next) {
-  console.log(req.body)
+  console.log(req.body);
   let recipe = new Recipe({
     title: req.body.title,
     products: req.body.products,
@@ -31,8 +43,20 @@ router.post("/recipes", function(req, res, next) {
       console.log("error!!!");
       return next(err);
     }
-    res.json(recipe);
+    res.json({recipe});
   });
 });
+
+router.delete("/recipes/:title", function(req, res, next){
+  Recipe.deleteOne({
+    title: req.params.tile
+  }, function(err, recipe){
+    if(err){
+      return next(err);
+    }
+    res.json({text:"Deleted"})
+  })
+})
+
 
 module.exports = router;
